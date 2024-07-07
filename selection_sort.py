d0c2c3966269620b607332e765b0115f6907c790
Mala732 <113810951+Mala732@users.noreply.github.com>
2024-07-07 10:11:46 +0530

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,15 @@
+arr = [15,12,4,14,2]
+
+for i in range(len(arr)):
+    min_index = i
+    for j in range(i+1,len(arr)):
+        if arr[j] < arr[min_index]:
+            min_index = j
+    
+    
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+    # temp = arr[i]
+    # arr[i] = arr[min_index]
+    # arr[min_index] = temp
+    
+print(arr)
